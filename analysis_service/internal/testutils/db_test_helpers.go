23d8e4fd337aff9e7fe5104a11ec63e7b162e497
Zaharazov <jack18283@mail.ru>
2025-06-03 18:29:59 +0300

    test fixes

---
+++ analysis_service/internal/testutils/db_test_helpers.go
@@ -0,0 +1,37 @@
+package testutils
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+)
+
+var TestDB *sql.DB
+
+func CleanTables(t *testing.T, db *sql.DB) {
+	t.Helper()
+
+	_, err := db.Exec("SET session_replication_role = 'replica'")
+	if err != nil {
+		t.Fatalf("failed to disable foreign key checks: %v", err)
+	}
+
+	tables := []string{
+		"fact_listens",
+		"songs",
+		"artists",
+		"genres",
+	}
+
+	for _, table := range tables {
+		_, err := db.Exec(fmt.Sprintf("TRUNCATE TABLE %s RESTART IDENTITY CASCADE", table))
+		if err != nil {
+			t.Fatalf("failed to truncate table %s: %v", table, err)
+		}
+	}
+
+	_, err = db.Exec("SET session_replication_role = 'origin'")
+	if err != nil {
+		t.Fatalf("failed to re-enable foreign key checks: %v", err)
+	}
+}
